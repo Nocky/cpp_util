40000a08e00873e18867fdd7b305b52487e16c46
chenguolin <cgl1079743846@gmail.com>
2016-05-14 01:49:19 +0800

    delete useless dir

--- test/mutex_lock_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "mutex_lock.h"
-
-using namespace Util;
-
-TEST(TestMutexLock, MutexLockGuard) {
-    MutexLock mutexLock;
-    MutexLockGuard mutexLockGuard(mutexLock);
-}
